beb6a8d5b04b0f495a3b3038062b7161d8cfee6b
goh-chunlin <chunlin@live.com.sg>
2017-08-23 02:51:42 +0800

    Formatted document.

--- Services/FileProcessor.cs
+++ Services/FileProcessor.cs
@@ -48,7 +48,8 @@ namespace Seayo.Services
 			var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
 
 			var blobClient = storageAccount.CreateCloudBlobClient();
-                blobClient.DefaultRequestOptions.RetryPolicy = new LinearRetry(TimeSpan.FromSeconds(3), 3);
+            
+			blobClient.DefaultRequestOptions.RetryPolicy = new LinearRetry(TimeSpan.FromSeconds(3), 3);
 
 			var blobContainer = blobClient.GetContainerReference(containerName);
 
